37036291649de55f5f5a2ad41108c3e04d66d88d
Alfredo Sobrados <alfredo.118s347@gmail.com>
2026-01-12 22:07:26 +0000

    Add ecosystem.config.js for PM2

---
+++ ecosystem.config.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  apps: [{
+    name: 'human-app',
+    script: './index.js',
+    instances: 1,
+    exec_mode: 'cluster',
+    env_production: {
+      NODE_ENV: 'production',
+      PORT: 8085
+    },
+    error_file: './logs/err.log',
+    out_file: './logs/out.log',
+    log_date_format: 'YYYY-MM-DD HH:mm:ss',
+    merge_logs: true
+  }]
+};
