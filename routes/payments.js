c70d285635142d516576cebe661419eaf300ab57
Alfredo Sobrados <alfredo.118s347@gmail.com>
2026-01-15 20:51:36 +0100

    little change

--- routes/payments.js
+++ routes/payments.js
@@ -2,10 +2,17 @@ import { Router } from 'express';
 const router = Router();
 import { verifyToken } from '../middlewares/verifyToken.js';
 import {initiatePayment, confirmPayment, paymentResultRedirect} from '../controllers/payments.js';
+import {generateHppUrl, handleHppResponse, listPaymentMethods, processGooglePay} from "../controllers/hpp.js";
 
 router.post('/initiate',verifyToken, initiatePayment);
 router.post('/status', verifyToken, confirmPayment); // statusURL (server-to-server)
-router.get('/result', verifyToken, paymentResultRedirect); // redirect URL
+router.get('/result', verifyToken, paymentResultRedirect);
+
+router.get('/hpp-url', generateHppUrl);
+router.post('/hpp-response', handleHppResponse);
+router.get('/hpp-response', handleHppResponse);
+router.post('/googlepay', processGooglePay);
+router.get('/methods', listPaymentMethods);// redirect URL
 
 
 router.get('/', async (req, res) => {
