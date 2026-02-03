b862d12e811ea4c1a96faccf94e5e4183b35aa89
Alfredo Sobrados <alfredo.118s347@gmail.com>
2026-02-03 11:17:48 +0100

    chore: deploy to production

--- config/stripe.config.js
+++ config/stripe.config.js
@@ -5,7 +5,7 @@ if (!process.env.STRIPE_SECRET_KEY) {
 }
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-    apiVersion: '2026-01-28',
+    apiVersion: '2026-01-28.clover',
 });
 
 export default stripe;
