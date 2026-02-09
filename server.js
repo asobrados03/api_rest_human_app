80489a041ac0f3fbcb6d779d1ec9c71058407f0e
Alfredo Sobrados <alfredo.118s347@gmail.com>
2026-02-09 18:50:00 +0100

    chore: deploy to production

--- server.js
+++ server.js
@@ -3,11 +3,14 @@ import app from './app.js';
 import { createServer } from 'http';
 
 const PORT = process.env.PORT || 8085;
-const HOST = '0.0.0.0';   // imprescindible en Azure/Caddy
+const HOST = '0.0.0.0';
 
 const server = createServer(app);
 
+server.keepAliveTimeout = 10000;     // 10 segundos (recomendado)
+server.headersTimeout   = 12000;     // debe ser mayor que keepAliveTimeout
+
 server.listen(PORT, HOST, () => {
-    console.log(`API REST corriendo en https://${HOST}:${PORT}`);
+    console.log(`API REST corriendo en http://${HOST}:${PORT}`);
     console.log(`Caddy debería estar proxy_pass → http://127.0.0.1:${PORT}`);
 });
